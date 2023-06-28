25925de2cb597ecfb291cde234c146efa5a7bc2e
aromalnambiar <aromalndr@gmail.com>
2023-06-28 12:29:07 +0530

    loading screen done

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,12 +2,12 @@ import { SunIcon } from "@heroicons/react/solid";
 
 function Loading() {
 	return (
-		<div className="bgs-gradient min-h-screen flex flex-col items-center justify-center text-slate-500">
+		<div className="bgs-gradient min-h-screen flex flex-col items-center justify-center text-white">
 			<SunIcon
-				className="h-24 w-24 animate-bounce text-yellow-500"
+				className="h-24 w-24 animate-bounce stroke-white text-yellow-500"
 				color="yellow"
 			/>
-			<h1 className="text-6xl font-bold text-center mb-10 animate-pulse">
+			<h1 className="text-6xl font-bold text-center mb-10 animate-pulse ">
 				Loading City Weather Information
 			</h1>
 			<h2 className="text-xl font-bold text-center mb-10 animate-pulse">
